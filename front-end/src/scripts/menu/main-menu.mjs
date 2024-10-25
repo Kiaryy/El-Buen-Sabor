2a183527c6589ecd630707acff9adedeab391308
Augusto <otsuguariquelme@gmail.com>
2024-10-25 15:35:32 -0300

    changes of structure, local plate management logic and local admin ready

---
+++ front-end/src/scripts/menu/main-menu.mjs
@@ -0,0 +1,43 @@
+import { show_plates } from "./show-plates/show-plates.mjs";
+import { show_plates_local } from "./show-plates/local-plates/show-plate-local.mjs";
+// Selección de las secciones donde se mostrarán los productos
+const section_hamburguesa = document.querySelector('#section-hamburguesa');
+const section_pizza = document.querySelector('#section-pizza');
+const section_empanada = document.querySelector('#section-empanada');
+const section_ensalada = document.querySelector('#section-ensalada');
+const section_acompañamiento = document.querySelector('#section-acompañamiento');
+const section_postre = document.querySelector('#section-postre');
+
+
+
+
+
+// Al cargar la página
+window.onload = async function () {
+    let users = JSON.parse(localStorage.getItem('Users')) || [];
+    
+    let index = users.findIndex(u => u.state == true);
+    if (!index==-1) {
+        button_profile.innerHTML = 'Perfil';
+    }
+    const sectionMap = {
+        HAMBURGUESA: section_hamburguesa,
+        PIZZA: section_pizza,
+        EMPANADA: section_empanada,
+        ENSALADA: section_ensalada,
+        ACOMPAÑANMIENTO: section_acompañamiento,
+        POSTRE: section_postre
+    };
+       
+        console.log("Cargando productos...");
+        
+        // URL de la API para obtener los platos
+        if (!localStorage.getItem('Platos')) {
+        //   Realizar la solicitud GET a la API
+            show_plates(sectionMap)
+         }else{
+            show_plates_local(sectionMap)
+
+    }   
+       
+};
---
+++ front-end/src/scripts/registro-inicio/API/api_add.mjs
@@ -0,0 +1,49 @@
+export const api_add =async(email,password)=>{
+    let user_save={
+        name: "",
+        phoneNumber : 0,
+        addresses: [],
+        cards: [],
+        mail: "",
+        passWord:"",
+        pedido:[],
+        state:false,
+    };
+        try{
+            const response=await fetch('https://bsapi-latest.onrender.com/usuarios/findAll',{
+                method:'GET'
+            });
+            const data =  await response.json();
+            
+            console.log(email);
+            console.log(password);
+            
+            for (let user of data) {
+                if (user.mail==email && user.passWord==password) {
+                user_save.name=user.name
+                user_save.phoneNumber=user.phoneNumber
+                user_save.addresses.push(user.addresses)
+                user_save.mail=user.mail
+                user_save.passWord=user.passWord
+                user_save.state=true
+                if (localStorage.getItem('Users')) {
+                    let users = JSON.parse(localStorage.getItem('Users')) || [];
+                    users.push(user_save)
+                    localStorage.setItem('Users', JSON.stringify([user_save]));
+                }else{
+                    localStorage.setItem('Users', JSON.stringify([user_save]));
+                }
+                
+                return true
+           }
+            
+        }
+       return false
+        
+    } 
+    catch(error){
+            // Manejar errores
+            console.error('Hubo un problema con la solicitud:', error);
+    }
+
+}
